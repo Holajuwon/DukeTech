6477953c5fed588829f0e67fb6cf913d25745aa4
hola <henrijay00@gmail.com>
2020-02-04 16:55:09 +0100

    added login and authentication with reroute to booking page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 import {
   Paper,
   withStyles,
@@ -21,70 +23,100 @@ const styles = theme => ({
 
 const Login = props => {
   const { classes } = props;
+  const history = useHistory();
+
+  const [todo, setTodo] = useState({ username: "", password: "" });
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setTodo({ ...todo, [name]: value });
+    console.log(todo);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { username, password } = todo;
+    axios
+      .post("https://mighty-mesa-03143.herokuapp.com/login", {
+        username,
+        password
+      })
+      .then(res => {
+        console.log(res);
+        return history.push("/book");
+      })
+      .catch(err => console.log(err));
+  };
   return (
     <Paper className={classes.padding}>
-      <div className={classes.margin}>
-        <Grid container justify="center" style={{ marginTop: "10px" }}>
-          <Typography style={{ fontSize: 50 }}> Login </Typography>
-        </Grid>
-        <Grid container spacing={8} alignItems="flex-end">
-          <Grid item>
-            <Face />
+      <form onSubmit={handleSubmit}>
+        <div className={classes.margin}>
+          <Grid container justify="center" style={{ marginTop: "10px" }}>
+            <Typography style={{ fontSize: 50 }}> Login </Typography>
           </Grid>
-          <Grid item md={true} sm={true} xs={true}>
-            <TextField
-              id="username"
-              label="Username"
-              type="email"
-              fullWidth
-              autoFocus
-              required
-            />
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              <Face />
+            </Grid>
+            <Grid item md={true} sm={true} xs={true}>
+              <TextField
+                id="username"
+                label="Username"
+                type="text"
+                fullWidth
+                autoFocus
+                required
+                name="username"
+                onChange={handleChange}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container spacing={8} alignItems="flex-end">
-          <Grid item>
-            <Fingerprint />
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              <Fingerprint />
+            </Grid>
+            <Grid item md={true} sm={true} xs={true}>
+              <TextField
+                id="username"
+                label="Password"
+                type="password"
+                fullWidth
+                required
+                name="password"
+                onChange={handleChange}
+              />
+            </Grid>
           </Grid>
-          <Grid item md={true} sm={true} xs={true}>
-            <TextField
-              id="username"
-              label="Password"
-              type="password"
-              fullWidth
-              required
-            />
+          <Grid container alignItems="center" justify="space-between">
+            <Grid item>
+              <FormControlLabel
+                control={<Checkbox color="primary" />}
+                label="Remember me"
+              />
+            </Grid>
+            <Grid item>
+              <Button
+                disableFocusRipple
+                disableRipple
+                style={{ textTransform: "none" }}
+                variant="text"
+                color="primary"
+              >
+                Forgot password ?
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container alignItems="center" justify="space-between">
-          <Grid item>
-            <FormControlLabel
-              control={<Checkbox color="primary" />}
-              label="Remember me"
-            />
-          </Grid>
-          <Grid item>
+          <Grid container justify="center" style={{ marginTop: "10px" }}>
             <Button
-              disableFocusRipple
-              disableRipple
-              style={{ textTransform: "none" }}
-              variant="text"
+              variant="outlined"
               color="primary"
+              style={{ textTransform: "none" }}
+              onClick={handleSubmit}
+              disabled={!(todo.username.length >= 5)}
             >
-              Forgot password ?
+              Login
             </Button>
           </Grid>
-        </Grid>
-        <Grid container justify="center" style={{ marginTop: "10px" }}>
-          <Button
-            variant="outlined"
-            color="primary"
-            style={{ textTransform: "none" }}
-          >
-            Login
-          </Button>
-        </Grid>
-      </div>
+        </div>
+      </form>
     </Paper>
   );
 };
